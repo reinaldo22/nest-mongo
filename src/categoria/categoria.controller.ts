8437ca232c0040f2a48316875b5585674b305ce8
Reinaldo da Silva Vargas <rsv.snf17@dmz.org.br>
2022-11-22 19:02:31 -0300

    Funcionalidades de categoria crud

--- src/categoria/categoria.controller.ts
+++ src/categoria/categoria.controller.ts
@@ -6,11 +6,31 @@ import { CategoriaService } from './categoria.service';
 @Controller('categoria')
 export class CategoriaController {
 
-    constructor(private readonly categoriaService: CategoriaService){}
+    constructor(private readonly categoriaService: CategoriaService) { }
 
     @Post()
     @UsePipes(ValidationPipe)
-    async criaCategoria(@Body() categoriaDTO: CategoriaDTO): Promise<Categoria>{
+    async criaCategoria(@Body() categoriaDTO: CategoriaDTO): Promise<Categoria> {
         return await this.categoriaService.criaCategoria(categoriaDTO);
+    }
+
+    @Get()
+    async findAll() {
+        return await this.categoriaService.findAllCategoria();
+    }
+
+    @Get('/:categoria')
+    findOne(@Param('categoria') categoria: string) {
+        return this.categoriaService.findAllCategoriaId(categoria);
+    }
+
+    @Put('/:categoria')
+    update(@Param('categoria') categoria: string, @Body() body) {
+        return this.categoriaService.atualizaCategoria(categoria, body);
+    }
+
+    @Put('/:categoria')
+    addCourseCategoria(@Param('categoria') categoria: string, @Body() body) {
+        return this.categoriaService.atualizaCategoria(categoria, body);
     }
 }
--- src/categoria/categoria.service.ts
+++ src/categoria/categoria.service.ts
@@ -1,24 +1,60 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CategoriaDTO } from 'src/dto/categoriaDTO';
+import { courseDTO } from 'src/dto/courseDTO';
 import { Categoria } from 'src/interfacesDocument/categoriaDocument';
 
 @Injectable()
 export class CategoriaService {
 
     constructor(@InjectModel("Categoria") private readonly categoriaSchema: Model<Categoria>) { }
-    async criaCategoria(categoriaDTO: CategoriaDTO): Promise<Categoria>{
+    async criaCategoria(categoriaDTO: CategoriaDTO): Promise<Categoria> {
 
         const { categoria } = categoriaDTO;
 
-        const categoriaExists = await this.categoriaSchema.findOne({categoria}).exec();
-        if(categoriaExists){
+        const categoriaExists = await this.categoriaSchema.findOne({ categoria }).exec();
+        if (categoriaExists) {
             throw new BadRequestException(`Categoria ${categoria} já cadastrada!`);
         }
 
         const categoriaCariada = new this.categoriaSchema(categoriaDTO);
 
-        return await categoriaCariada.save(); 
+        return await categoriaCariada.save();
+    }
+
+    async findAllCategoria(): Promise<Array<Categoria>> {
+        return await this.categoriaSchema.find().populate('courses').exec()
+    }
+
+    async findAllCategoriaId(categoria: string): Promise<Categoria> {
+        const categoriaExists = await this.categoriaSchema.findOne({ categoria }).exec();
+
+        if (!categoriaExists) {
+            throw new BadRequestException(`Categoria ${categoria} não encontrada!`);
+        }
+
+        return categoriaExists;
+    }
+
+    async atualizaCategoria(categoria: string, categoriaDTO: CategoriaDTO): Promise<void> {
+        const categoriaExists = await this.categoriaSchema.findOne({ categoria }).exec();
+        if (!categoriaExists) {
+            throw new NotFoundException(`Categoria ${categoria} não encontrada!`)
+        }
+
+        await this.categoriaSchema.findOneAndUpdate({ categoria }, { $set: categoriaDTO });
+        
+    }
+
+    async addCourseCategoria(categoria:string, courses: courseDTO) {
+        
+        const categoriaExists = await this.categoriaSchema.findOne({ categoria }).exec();
+        if (!categoriaExists) {
+            throw new NotFoundException(`Categoria ${categoria} não encontrada!`)
+        }
+
+        await this.categoriaSchema.findOneAndUpdate({ categoria }, { $set: courses });
+
     }
 }
